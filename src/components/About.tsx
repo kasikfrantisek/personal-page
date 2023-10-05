d5bfc16fdbca20e2be5b2b61ee0070129a084363
kasikfrantisek <kasik.frantisek97@gmail.com>
2023-10-05 21:15:19 +0200

    fixes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ import { Bubble } from "./Bubble"
 
 export const About = () => {
     return (
-        <div className="w-full h-screen snap-start span-always text-white md:pt-28 md:py-28 pt-10 px-5 relative overflow-hidden">
+        <div className="w-full h-screen snap-start span-always text-white md:pt-28 md:pl-28 px-5 relative overflow-hidden">
             <h1 className="md:text-[100px] text-3xl uppercase font-bold leading-[100%] w-4/5 md:w-2/3 ">From cocktails to code</h1>
             <div className="md:w-2/3 md:text-xl text-sm leading-[120%] space-y-2 md:space-y-5 md:pt-10 pt-5 font-light tracking-wide">
             <p>After high school, I explored universities and spent some time living abroad. Eventually, I found my calling as a bartender, where I embraced the blend of creativity, order, and human interactions. However, the pandemic disrupted the stability of the hospitality industry, prompting me to shift my focus to digital screens. Driven by curiosity, I delved into coding to understand the inner workings of digital platforms.</p>
